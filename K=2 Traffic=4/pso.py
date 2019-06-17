84100c22cc50270d0d97bbb4ae1f1d5f50dabee6
Alejandro Román <aromannurena@gmail.com>
2019-06-16 20:26:33 -0500

    Fix population generation

--- K=2 Traffic=4/pso.py
+++ K=2 Traffic=4/pso.py
@@ -11,9 +11,43 @@
 
 from random import randrange
 
+class _Particle(object):
+	"""
+	Class that stores the information for a particle (a solution)
+	:param pos: A list of pairs. Each pair represents a traffic light's times
+	:param vel: A list of pairs. Each pair represents the velocity for the n-th traffic light in self.pos
+	"""
+
+	def __init__(self, trafficLightsTimes, max_time):
+		self.pos = trafficLightsTimes
+		self.vel = self.generateRandomVelocities(trafficLightsTimes, max_time)
+
+	def generateRandomVelocities(self, trafficLightsTimes, max_time):
+		"""
+		Returns a list of random velocities pair for each pair of times in trafficLightsTimes.
+		The velocities will comply the rule that states: (xTime + yTime = max_time).
+		"""
+		velocities = []
+		minPos = 1
+		maxPos = max_time - 1
+		for tl in trafficLightsTimes:
+			xPos = tl[0]
+			yPos = tl[1]
+
+			# Set a random velocity. The new velocity must not have an axis that is below minPos or is greater than maxPos
+			maxDelta = max(maxPos - xPos, maxPos - yPos, xPos - minPos, yPos - minPos)
+			xVel = randrange(-maxDelta, maxDelta)
+			if xPos + xVel < minPos or xPos + xVel > maxPos:
+				xVel = -xVel
+
+			# Remember that (x + y) must always sum max_time, so if x increases by n, y must decrease by n.
+			yVel = -xVel
+			velocities.append((xVel, yVel))
+		return velocities
+
 def init_population(num_particles, num_traffic_lights, max_time):
 	"""
-	Initializes population for pso algorithm for a 2-axes based traffic light.
+	Return an initialized population for the pso algorithm for a 2-axes based traffic light problem.
     num_particles:  Number of particles in population
     num_traffic_lights: Number of traffics lights in one solution
 	max_time: Max cicle time in seconds
@@ -21,25 +55,42 @@ def init_population(num_particles, num_traffic_lights, max_time):
 	minPos = 1
 	maxPos = max_time - 1
 	population = []
-	for i in range(num_particles):
-		# Set the time for each axes.
-		firstAxisPos = randrange(minPos, maxPos)
-		secondAxisPos = max_time - firstAxisPos
-		position = [firstAxisPos, secondAxisPos]
+	for particle in range(num_particles):
+		trafficLights = []
+		for tl in range(num_traffic_lights):
+			# Set the time for each axis. Remember that xPos + yPos must sum max_time
+			xPos = randrange(minPos, maxPos)
+			yPos = max_time - xPos
 
-		# Set a random velocity. The new velocity must not have an axis that is below minPos or is greater than maxPos
-		maxDelta = max(maxPos - firstAxisPos, maxPos - secondAxisPos, firstAxisPos - minPos, secondAxisPos - minPos)
-		firstAxisVel = randrange(-maxDelta, maxDelta)
-		if firstAxisPos + firstAxisVel < 1 or firstAxisPos + firstAxisVel > 89:
-			firstAxisVel = -firstAxisVel
+			trafficLights.append((xPos, yPos))
 
-		# Remember that x + y must always be max_time, so if x increases by n, y must decrease by n.
-		secondAxisVel = -firstAxisVel
-		velocity = [firstAxisVel, secondAxisVel]
-
-		# Store the new position-velocity pair
-		population.append((position, velocity))
+		# Will the list of pairs of times for each traffic light, create a new particle
+		p = _Particle(trafficLights, max_time)
+		population.append(p)
 	return population
 
-def pso_algorithm():
-	print('pso_algorithm')
+def pso_algorithm(population, fitness_fn, w, phi1, phi2, max_iter):
+	"""
+	Return the best population and the best fitness obtained for the traffic light optimization problem.
+    population:  The initial population
+    fitness_fn: The fitness function that will the maximized
+	w: Inertial factor
+	phi1: Individual factor
+	phi2: Social factor
+	max_iter: Maximum number of iterations that the search will have
+	"""
+	bestPopulation = []
+	bestFitness = -1
+
+	#for iteration in range(max_iter):
+
+
+	return bestPopulation, bestFitness
+
+def _printPopulation(population):
+	""" Debug function that prints a population """
+	for particle in population:
+		print("\nParticle")
+		print(particle.pos)
+		print(particle.vel)
+#_printPopulation(init_population(6,10,90))
